786fff69d873f644242b8405b13401962cab8ace
Sulavi <Sulavi@rocketmail.com>
2016-06-09 16:40:39 -0700

    Added Arduino Code

---
+++ Arduino Code/cubeRobot/cubeRobot.ino
@@ -0,0 +1,17 @@
+/*
+Cube-Robot Arduino 
+This takes commands over the serial port and outputs the correct commands to the servos
+Created June 9, 2016
+by Richard Arkusinski
+*/
+
+void setup(){
+  
+  Serial.begin(9600);
+  
+}
+
+void loop(){
+  
+  delay(1);
+}
